647164005e84dc37540296c7a48ce8988ce6d96d
JerryKerry <jerrykerry@mail.ru>
2022-03-22 15:18:55 +0300

    Выполненное задание - параметризация тестов

--- test_time.py
+++ test_time.py
@@ -1,25 +1,32 @@
+import math
+import time
+from pip import __main__
 import pytest
 from selenium.webdriver.common.by import By
-from selenium import webdriver
-import time
-import math
 
 
-@pytest.mark.parametrize('list', ["236895", "236896"]) #236897 , 236898 , 236899 , 236903,236904 ,236905"])
-class Test():
+@pytest.mark.parametrize('list', ["236895", "236896", '236897' , '236898' , '236899' , '236903','236904' ,'236905']  )
 
-    def test_guest_should_see_login_link(self, browser, list):
+
+
+
+class Test:
+
+    def test_url(self, browser, list):
         answer = str(math.log(int(time.time())))
-
-
 
         link = f"https://stepik.org/lesson/{list}/step/1"
         browser.get(link)
-        browser.implicitly_wait(15)
-        browser.find_element(By.CSS_SELECTOR, "[id='ember86']").send_keys(answer)
+
+        browser.find_element(By.CLASS_NAME, 'ember-text-area.ember-view.textarea.string-quiz__textarea').send_keys(answer)
 
         browser.find_element(By.CLASS_NAME, 'submit-submission').click()
-        #alert = browser.switch_to.alert
-        #alert_text = alert.text
-        #assert alert_text == 'Correct'
+
+        welcome_text = browser.find_element(By.CLASS_NAME, "smart-hints__hint")
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        assert "Correct!" in welcome_text.text
         time.sleep(5)
+
+
+if __main__ == __name__:
+    pytest.main()
