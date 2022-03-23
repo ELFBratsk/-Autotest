d054b2219eda59e21c42515d170975aef32dcd5a
JerryKerry <jerrykerry@mail.ru>
2022-03-23 11:17:23 +0300

    сохранение

--- Test_conftest/conftest.py
+++ Test_conftest/conftest.py
@@ -1,13 +1,26 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-@pytest.fixture
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome(r'C:\\Users\\user\\PycharmProjects\\data_test\\chromedriver.exe')
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(r'C:\\Users\\user\\PycharmProjects\\data_test\\chromedriver.exe')
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(r'C:\geckodriver')
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
-
-
 
